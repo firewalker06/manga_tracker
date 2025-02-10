375af81cd1f579fa66b12c282ab1fc611b30172a
firewalker06 <didik.wicaksono@gmail.com>
2025-02-10 12:40:08 +0700

    Remove inflections

--- app/models/manga_tracker/manga.rb
+++ app/models/manga_tracker/manga.rb
@@ -1,5 +1,7 @@
 module MangaTracker
   class Manga < ApplicationRecord
+    self.table_name = "manga_tracker_manga"
+
     has_many :chapters, dependent: :destroy
   end
 end
--- test/dummy/config/initializers/inflections.rb
+++ test/dummy/config/initializers/inflections.rb
@@ -15,6 +15,6 @@
 #   inflect.acronym "RESTful"
 # end
 
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.uncountable "manga"
-end
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.uncountable "manga"
+# end
